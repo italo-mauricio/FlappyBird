1c4aae8ac57d523b9e4463e4a3e7471c44707ed9
italo <italogithub@gmail.com>
2022-10-09 12:47:36 -0300

    update

--- flappy.py
+++ flappy.py
@@ -3,21 +3,32 @@ from pygame.locals import *
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 700
-
+SPEED = 10
 
 class Bird(pygame.sprite.Sprite): # Definindo a classe do pássaro
     
     def __init__(self): # init padrão da OO
         pygame.sprite.Sprite.__init__(self)
         
-        self.image = pygame.image.load('bluebird-midflap.png').convert_alpha()  # convert_alpha vai ignorar o pixels que se sobressaem na imagem
+        self.images = [pygame.image.load('bluebird-upflap.png').convert_alpha(),
+                       pygame.image.load('bluebird-midflap.png').convert_alpha(),
+                       pygame.image.load('bluebird-downflap.png').convert_alpha(),
+                       ]
+        
+        self.current_image = 0
+        
+        self.image = pygame.image.load('bluebird-upflap.png').convert_alpha()  # convert_alpha vai ignorar o pixels que se sobressaem na imagem
+
         self.rect = self.image.get_rect()
         self.rect[0] = SCREEN_WIDTH / 2                                         # colocando o pássaro no meio da tela
         self.rect[1] = SCREEN_HEIGHT / 2
         
          
     def update(self):
-        pass
+        
+        self.current_image = (self.current_image + 1) % 3
+        self.image = self.images [ self.current_image ]
+        
     
     
     
@@ -32,9 +43,10 @@ bird_group = pygame.sprite.Group()
 bird = Bird()
 bird_group.add(bird)
 
+clock = pygame.time.Clock()
 
 while True:
-    
+    clock.tick(20)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
